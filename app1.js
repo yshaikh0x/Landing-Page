39871d7e16845b4412b5b11ba77e4a522f263396
yshaikh0x <77468394+yshaikh0x@users.noreply.github.com>
2021-03-07 20:05:14 -0500

    Add files via upload

--- app1.js
+++ app1.js
@@ -19,26 +19,34 @@
 */
 const navigation = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
-const navLink = document.querySelectorAll('li');
+const navItem = document.querySelectorAll('li');
 let allNavLink = document.querySelectorAll('li ul navbar__menu a')
 const active = document.getElementsByClassName("active")
 let activeNav = document.querySelector('.active-nav');
 
 
-// build the nav
+// Build the Navigation
 sections.forEach(section => {
   const docFrag = document.createDocumentFragment();
   const navItem = document.createElement("li");
   const navItemLink = document.createElement("a");
   navItem.classList.add(section.id);
   navItemLink.textContent = section.id;
-  navItem.setAttribute('data-id', section.id)
+  navItem.setAttribute('data-id', section.id);
   navItem.id = `nav-${section.id}`;
-  navItemLink.href =`#${section.id}`;
+ // Adding smooth scrolling
+ navItem.addEventListener('click', event => {
+  section.scrollIntoView({
+    behavior:"smooth",
+    block: "start"
+  });
+});
   navItem.appendChild(navItemLink);
   docFrag.appendChild(navItem);
   navigation.appendChild(docFrag);
 })
+
+
 // Add class 'active' to section when near top of viewport
 function checkView(section){
     let position = section.getBoundingClientRect();
@@ -47,32 +55,21 @@ function checkView(section){
 
 //Active class on scroll
 window.addEventListener('scroll', event => {
-  
   sections.forEach(section => { 
     if (checkView(section)){
-      //adds active-nav on scroll
+      //Adds active-nav on scroll
       let id = section.getAttribute('id');
       document.querySelector(`.${id}`).classList.add('active-nav');
       //Adds active class to section
         section.classList.add('active');
     } else {
-       //remove active-nav on scroll
+       //Removes active-nav on scroll
       let id = section.getAttribute('id');
       document.querySelector(`.${id}`).classList.remove('active-nav')
-      //Remove active class to section
+      //Removes active class to section
       section.classList.remove('active');
-      //Smooth Scrolling Event
-      sections.addEventListener('click', event => {
-        sections.scrollIntoView({
-          behavior: "smooth",
-          block: "start"
-        });
-      });
     } 
-    
   });
-  
   });
-
 
  
